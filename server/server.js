aed41778a35239994532af5ed0d71dc0dde10e38
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-12 22:47:15 -0700

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-
+const { typeDefs, resolvers } = require('./schemas');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const { authMiddleware } = require('./utils/auth');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,9 +19,20 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({app});
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`Runnin on localhost:${PORT}`);
+      console.log(
+        `GraphQL http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+startApolloServer();
